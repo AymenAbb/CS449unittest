ae7dba06f1c42ba10e4c64ce5c77132a9fc487b0
Aymen <123590232+AymenAbb@users.noreply.github.com>
2025-09-09 12:39:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import trains
+
+if __name__ == "__main__":
+    print(trains.get_route_score(9))
+    print(trains.get_route_score(-10))
+    print(trains.get_route_score(-0))
+    print(trains.get_route_score(99))
+    print(trains.get_route_score(1))
